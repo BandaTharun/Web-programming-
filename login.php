17231ed54436f9d92e3c737d1473954db148f837
BANDA THARUN REDDY <99380789+BandaTharun@users.noreply.github.com>
2023-02-16 14:13:24 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,172 @@
+
+
+<?php
+// Check if condition is true
+
+  // Collect form data
+  $form_data = array(
+    'name' => $_POST['name'],
+    'email' => $_POST['email'],
+    'message' => $_POST['message']);
+
+  // Send form data to desired page using POST method
+  $url = "Location: chk.php" ;
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $form_data);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($curl);
+  curl_close($curl);
+
+
+
+
+// collecting form data using $_POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = ( $_POST['email']);
+    $password =( $_POST['password']);
+    echo $email;
+    echo $password;
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+// connecting to the DB
+include 'db_connect.php';
+
+// collecting form data using $_POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    // checking the form data
+    if (empty($email) || empty($password))  {
+        //echo "The email and password is not filled out completely.";
+    } else {
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            //echo "Access granted.";
+
+            $sql = "SELECT * FROM information_schema.tables WHERE table_schema = 'mydb8' AND table_name = '$email' ";  
+      
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                echo "Table exists";
+                  // Collect form data
+                  $form_data = array('email' => $email,'password' => $password);
+                  // Send form data to desired page using POST method
+                  $url = 'home.php';
+                  $curl = curl_init($url);
+                  curl_setopt($curl, CURLOPT_POST, true);
+                  curl_setopt($curl, CURLOPT_POSTFIELDS, $form_data);
+                  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                  $response = curl_exec($curl);
+                  curl_close($curl);
+                
+                // submiting to the home file
+                header("Location: home.php");
+
+            } else {
+                echo "Table does not exist";
+                 $sql = "CREATE TABLE  `" . $email . "`  (id INT(100) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    Title VARCHAR(100) NOT NULL,
+                    Descriptions VARCHAR(100) NOT NULL,
+                    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
+                
+                if (mysqli_query($conn, $sql)) {
+                    echo "Table created successfully";
+                      // Collect form data
+                      $form_data = array('email' => $email,'message' => $password);
+                      // Send form data to desired page using POST method
+                      $url = 'home.php';
+                      $curl = curl_init($url);
+                      curl_setopt($curl, CURLOPT_POST, true);
+                      curl_setopt($curl, CURLOPT_POSTFIELDS, $form_data);
+                      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                      $response = curl_exec($curl);
+                      curl_close($curl);
+                      
+                    // submiting to the home file
+                    header("Location: home.php");
+
+                } else {
+                    echo "Error creating table: " . mysqli_error($conn);
+                }
+            }
+
+            
+        } else {
+            echo "<h1>YOU HAVE ENTERED INCORRECT LOGIN DETAILS TRY AGAIN </h1>";
+        }
+    }
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/signup.css">
+<title>NOTEBOOK- Login</title>
+<style>
+ 
+</style>
+</head>
+<body>
+
+ <form action="<?php echo $_SERVER['PHP_SELF'];?>"  method="post" style="border:1px solid #ccc" >
+   <div class="container">
+     <h1> Welcome Back !! Login  And  Start Using. </h1>
+     <p>Fill and start using .</p>
+   
+       <label for="email"><b>Email</b></label>
+       <div class="allinput">
+         <input type="text" placeholder="Enter Email" name="email" required>
+       </div>
+
+       <label for="psw"><b>Password</b></label>
+       <div class="allinput">
+         <input type="password" placeholder="Enter Password" name="password" required>
+       </div>
+       
+       <p>Signup to create accocunt <a href="Signup.php" style="color:dodgerblue">CLICK HERE </a>.</p>
+
+       <div class="clearfix">
+         <button type="submit" class="signupbtn" >Login </button>
+       </div>
+     </div>  
+   </div>
+ </form>
+
+</body>
+</html>
+
